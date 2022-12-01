f2a780ee7eee66608e611ecf531dcc1f97b3b049
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-30 21:15:55 -0500

    Added background

---
+++ WalletAnimation/View/Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home.swift
+//  WalletAnimation
+//
+//  Created by Sergey Lukaschuk on 2022-11-30.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top) 
+        .background(Color.black)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
