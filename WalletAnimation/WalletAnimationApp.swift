de8fe99f1c00b0c90d28515ec4de01fb091adba0
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-30 21:02:27 -0500

    Initial Commit

---
+++ WalletAnimation/WalletAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAnimationApp.swift
+//  WalletAnimation
+//
+//  Created by Sergey Lukaschuk on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct WalletAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
