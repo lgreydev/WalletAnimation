fe7659deb74d8a1e22c5e00429f8ad02d2f5a818
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-01 20:52:15 -0500

    Added header view

--- WalletAnimation/View/Home.swift
+++ WalletAnimation/View/Home.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct Home: View {
+
     var body: some View {
         VStack {
+            HeaderView()
 
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top) 
+        .padding(15)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(Color.black)
     }
 }
+
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            Text("My Card")
+                .font(.title.bold())
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Button {
+
+            } label: {
+                Image(systemName: "plus")
+                    .font(.title2.bold())
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.white)
+                    }
+            }
+        }
+    }
+}
+
+
+
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
